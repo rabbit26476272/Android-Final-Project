12be8029ea753af1f8e866d353c0b1ba5db5a5d9
anan <a26476272@gmail.com>
2020-06-12 20:26:01 +0800

    no message

---
+++ recipe/app/src/main/java/com/manjurulhoque/myrecipes/fragment/HomeTabFragment.java
@@ -0,0 +1,112 @@
+package com.manjurulhoque.myrecipes.fragment;
+
+import android.app.Activity;
+import android.graphics.PorterDuff;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.view.ViewPager;
+import android.support.v7.widget.Toolbar;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.manjurulhoque.myrecipes.activity.MainActivity;
+import com.manjurulhoque.myrecipes.R;
+import com.manjurulhoque.myrecipes.adapter.TabPagerAdapter;
+
+public class HomeTabFragment extends Fragment {
+
+    public TabLayout tabLayout;
+    public ViewPager viewPager;
+    private MainActivity mainActivity;
+    private Toolbar toolbar;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View v = inflater.inflate(R.layout.fragment_home_tab, container, false);
+
+        toolbar = v.findViewById(R.id.tab_toolbar);
+        setupToolbar();
+
+        tabLayout = v.findViewById(R.id.tabs);
+        viewPager = v.findViewById(R.id.viewpager);
+
+        viewPager.setAdapter(new TabPagerAdapter(getChildFragmentManager()));
+
+        tabLayout.post(new Runnable() {
+            @Override
+            public void run() {
+                tabLayout.setupWithViewPager(viewPager);
+
+                tabLayout.setTabTextColors(getResources().getColor(R.color.tabTextColor),
+                        getResources().getColor(R.color.colorAccent));
+                int[] tabIcons = {
+                        R.drawable.recipe,
+                        R.drawable.spoon,
+                };
+                for(int i=0; i< tabLayout.getTabCount(); i++){
+                    if(tabLayout.getTabAt(i) != null){
+                        tabLayout.getTabAt(i).setIcon(tabIcons[i]);
+                    }
+                }
+                tabLayout.getTabAt(0).getIcon().setColorFilter(getResources().getColor(R.color.colorAccent), PorterDuff.Mode.SRC_IN);
+                tabLayout.getTabAt(1).getIcon().setColorFilter(getResources().getColor(android.R.color.white), PorterDuff.Mode.SRC_IN);
+            }
+        });
+
+        viewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+                switch (position) {
+                    case 0:
+                        toolbar.setTitle("RECIPES");
+                        tabLayout.getTabAt(0).getIcon().setColorFilter(getResources().getColor(R.color.colorAccent), PorterDuff.Mode.SRC_IN);
+                        tabLayout.getTabAt(1).getIcon().setColorFilter(getResources().getColor(android.R.color.white), PorterDuff.Mode.SRC_IN);
+                        break;
+                    case 1:
+                        toolbar.setTitle("CATEGORIES");
+                        tabLayout.getTabAt(1).getIcon().setColorFilter(getResources().getColor(R.color.colorAccent), PorterDuff.Mode.SRC_IN);
+                        tabLayout.getTabAt(0).getIcon().setColorFilter(getResources().getColor(android.R.color.white), PorterDuff.Mode.SRC_IN);
+                        break;
+                }
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+
+            }
+
+        });
+
+        return v;
+    }
+
+    public HomeTabFragment() {
+    }
+
+    @Override
+    public void onAttach(Activity activity) {
+        super.onAttach(activity);
+        mainActivity = (MainActivity) activity;
+    }
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        mainActivity.setupNavigationDrawer(toolbar);
+    }
+
+    private void setupToolbar() {
+        toolbar.setTitle(getString(R.string.app_name));
+        mainActivity.setSupportActionBar(toolbar);
+    }
+}
